2a144145d4e96e0ea1071c9137f5f0bef4ba40a9
mildsummer <mild.summer.y@gmail.com>
2019-11-08 17:27:43 +0900

    [update]AsyncMiddlewareをasync/awaitに修正

--- js/AsyncMiddleware.js
+++ js/AsyncMiddleware.js
@@ -1,53 +1,53 @@
 import { Alert } from "react-native";
 
-export default store => next => action => {
+export default store => next => async action => {
+  next(action);
   if (action.async) {
     const asyncOptions =
       typeof action.async === "function" ? action.async(store) : action.async;
     let promise = null;
     if (asyncOptions.dbRef) {
+      // firestore
       promise = asyncOptions.dbRef[asyncOptions.dbMethod || "get"](
         ...(asyncOptions.args || [])
       );
     } else if (asyncOptions.func) {
+      // function that returns promise
       promise = asyncOptions.func(...(asyncOptions.args || []));
     }
-    promise
-      .then((...results) => {
-        let data = null;
-        if (typeof asyncOptions.data === "function") {
-          data = asyncOptions.data(...results);
-        } else if (asyncOptions.data) {
-          data = asyncOptions.data;
-        } else if (results.length === 1) {
-          data = results[0];
-        } else if (results.length) {
-          data = results;
-        }
-        next({
-          type: `${action.type}/SUCCESS`,
-          data
-        });
-        if (typeof asyncOptions.success === "function") {
-          asyncOptions.success(...results);
-        } else if (typeof asyncOptions.success === "object") {
-          next(asyncOptions.success);
-        }
-      })
-      .catch(error => {
-        next({
-          type: `${action.type}/FAIL`,
-          error
-        });
-        if (asyncOptions.alertOnError) {
-          Alert.alert(asyncOptions.alertOnError || error.message);
-        }
-        if (typeof asyncOptions.fail === "function") {
-          asyncOptions.fail(error);
-        } else if (typeof asyncOptions.fail === "object") {
-          next(asyncOptions.fail);
-        }
+    try {
+      const result = await promise;
+      let data = null;
+      if (typeof asyncOptions.data === "function") {
+        data = asyncOptions.data(result);
+      } else if (asyncOptions.data) {
+        data = asyncOptions.data;
+      } else {
+        data = result;
+      }
+      next({
+        type: `${action.type}/SUCCESS`,
+        data
       });
+      if (typeof asyncOptions.success === "function") {
+        asyncOptions.success(result);
+      } else if (typeof asyncOptions.success === "object") {
+        next(asyncOptions.success);
+      }
+    } catch (error) {
+      console.error(error);
+      next({
+        type: `${action.type}/FAIL`,
+        error
+      });
+      if (asyncOptions.alertOnError) {
+        Alert.alert(asyncOptions.alertOnError || error.message);
+      }
+      if (typeof asyncOptions.fail === "function") {
+        asyncOptions.fail(error);
+      } else if (typeof asyncOptions.fail === "object") {
+        next(asyncOptions.fail);
+      }
+    }
   }
-  next(action);
 };
